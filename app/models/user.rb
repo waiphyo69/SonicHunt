c697c5c2e2afd4989e8bb117e2c430794b0b3677
waiphyo69 <waiphyo300@gmail.com>
2015-07-12 17:46:50 -0400

    Complete user auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,39 @@
 class User < ActiveRecord::Base
+  EMAIL_FORMAT = /^([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})$/i
+  attr_reader :password
+  validates :username, :email, :password_digest, :session_token, presence: true
+  validates :username, :email, :password_digest, :session_token, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :username, length: { maximum: 15 }
+  validates_format_of :email, with: EMAIL_FORMAT, multiline: true
+
+  after_initialize :ensure_session_token
+
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil unless user && user.valid_password?(password)
+    user
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def valid_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
+
 end
