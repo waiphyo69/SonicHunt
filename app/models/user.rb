053b80ef01181e614a1c5868957c8162a60e2662
waiphyo69 <waiphyo300@gmail.com>
2015-07-12 22:28:31 -0400

    Create products model and migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ActiveRecord::Base
 
   after_initialize :ensure_session_token
 
+  
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     return nil unless user && user.valid_password?(password)
---
+++ db/migrate/20150713021720_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :image_url, null: false
+      t.string :name, null: false
+      t.text :info, null: false
+      t.integer :avg_score, default: 0
+
+      t.timestamps null: false
+    end
+    add_index :products, :name, unique: true
+  end
+end
