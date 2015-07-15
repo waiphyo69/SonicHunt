cba8052bc5497e3d312d0666ed0c01e7355d71a6
waiphyo69 <waiphyo300@gmail.com>
2015-07-15 10:29:36 -0400

    Add create action to rails review controller

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -9,5 +9,20 @@ module Api
       @review = Review.find(params[:id])
       render :show
     end
+
+    def create
+      @review = Review.new(review_params)
+      if @review.save
+        render json: @review
+      else
+        render json: @review.errors.full_messages, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def review_params
+      params.require(:review).permit(:review_id, :owner_id, :created_at, :upvoted_at)
+    end
   end
 end
